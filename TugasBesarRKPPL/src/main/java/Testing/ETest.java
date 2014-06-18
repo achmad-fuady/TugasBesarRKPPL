745a67be527a8363ee3becf56407a3eb7e2adb8f
Rezza <rezza.kurniawan@gmail.com>
2014-06-18 21:43:36 +0700

    Bagian Rezza Selesai

---
+++ TugasBesarRKPPL/src/main/java/Testing/ETest.java
@@ -0,0 +1,42 @@
+/**
+ * Dibuat Oleh Rezza
+ */
+package Testing;
+
+import java.awt.List;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import Class.E;
+
+/**
+ * @author Rezza Kurniawan - (Erka)
+ * Class Testing
+ */
+public class ETest {
+
+	@BeforeClass
+	public void setUp() {
+		System.out.println("Pengujian Class E");
+	}
+	
+	@Test(groups = { "ETest" })
+	public void containsTest() {
+		E callE = new E();
+		Assert.assertEquals(callE.contains("napoleon"), true);
+	}
+	
+	@Test(groups = { "ETest" })
+	public void ListTest() {
+		E callE = new E();
+		
+		List actual = new List();
+		actual.add("List 1");
+		actual.add("List 2");
+		callE.setDataList(actual);
+		
+		Assert.assertSame(callE.getDataList().countItems(), 2);
+	}
+	
+}
--- TugasBesarRKPPL/src/main/java/Testing/PowTesting.java
+++ TugasBesarRKPPL/src/main/java/Testing/PowTesting.java
@@ -25,4 +25,10 @@ public class PowTesting {
 		Assert.assertEquals(callD.pow(2, 3), 8.0,"Gagal");
 	}
 	
+	@Test(groups = {"Pow"})
+	public void DStringTest() {
+		D callD = new D();
+		Assert.assertEquals(callD.toString(), "Ini Adalah toString dari Class D Implement PowInterface");
+	}
+	
 }
