e2d3f0e2d0c9bbc1bdb9175e22b607cc82d03fd9
achmad-fuady <tanjung.achmad.fuady@gmail.com>
2014-06-19 08:41:22 +0700

    Create CTest.java

---
+++ TugasBesarRKPPL/src/test/java/Testing/CTest.java
@@ -0,0 +1,50 @@
+package Testing;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Class.C;
+
+public class CTest {
+  /**
+   * @author Achmad Fuady
+   */
+	@BeforeClass
+	public void setUp() {
+		System.out.println("Pengujian Class E");
+	}
+	
+	@Test(groups = { "C" })
+	public void addTest() {
+		C c = new C();
+		Assert.assertEquals(c.add(), 6);
+	}
+	@Test(groups = { "C" })
+	public void subTest() {
+		C c = new C();
+		Assert.assertEquals(c.sub(), 2);
+	}
+	
+	@Test(groups = { "C" })
+	public void mulTest() {
+		C c = new C();
+		Assert.assertEquals(c.mul(), 8);
+	}
+	@Test(groups = { "C" })
+	public void divTest() {
+		C c = new C();
+		Assert.assertEquals(c.div(), 2);
+	}
+	
+	@Test(groups = { "C" })
+	public void modTest() {
+		C c = new C();
+		Assert.assertEquals(c.mod(4,2), 0);
+	}
+	@Test(groups = { "C" })
+	public void sqrtTest() {
+		C c = new C();
+		Assert.assertEquals(c.sqrt(8), 2.8284271247461903);
+	}
+}
