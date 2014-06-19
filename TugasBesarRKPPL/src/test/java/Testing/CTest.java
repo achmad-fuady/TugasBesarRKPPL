d4bc2fa96857fad7f5b9cf5b41815b5254c9be16
achmad-fuady <tanjung.achmad.fuady@gmail.com>
2014-06-19 12:18:59 +0700

    Update CTest.java

--- TugasBesarRKPPL/src/test/java/Testing/CTest.java
+++ TugasBesarRKPPL/src/test/java/Testing/CTest.java
@@ -29,12 +29,12 @@ public class CTest {
 	@Test(groups = { "C" })
 	public void mulTest() {
 		C c = new C();
-		Assert.assertEquals(c.mul(), 8);
+		Assert.assertEquals(c.mul(), 8.0);
 	}
 	@Test(groups = { "C" })
 	public void divTest() {
 		C c = new C();
-		Assert.assertEquals(c.div(), 2);
+		Assert.assertEquals(c.div(), 2.0);
 	}
 	
 	@Test(groups = { "C" })
